c16b53a2bfd395c828c62914357c06c6959a4e03
Julian Greco <julian_greco@hotmail.com>
2022-02-24 23:05:38 -0300

    Proyecto finalizado.

---
+++ src/main/java/com/alkemy/disney/auth/entity/UserEntity.java
@@ -0,0 +1,96 @@
+package com.alkemy.disney.auth.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user")
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Email
+    private String username;
+    @Size(min = 8)
+    private String password;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialNonExpired;
+    private boolean enabled;
+
+    public UserEntity(){
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialNonExpired = true;
+        this.enabled = true;
+    }
+
+    public Long getId(){return id;}
+
+    public void setId(Long id){this.id = id;}
+
+    @Override
+    public String getUsername(){return username;}
+
+    public void setUsername(String username){this.username = username;}
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialNonExpired;
+    }
+
+    public void setCredentialNonExpired(boolean credentialNonExpired) {
+        this.credentialNonExpired = credentialNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+
+}
